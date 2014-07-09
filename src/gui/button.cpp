639086bcfb2ed2bebe3b1fcdd71258a5ce4d24cf
aoisensi <aoisensi@aoisensi.info>
2014-07-09 15:55:04 +0900

    remove module

--- src/gui/button.cpp
+++ src/gui/button.cpp
@@ -17,8 +17,15 @@ void Button::LeaveCursor() { rideing = false; };
 void Button::MoveCursor(Point pos) { };
 
 void Button::Rendering(SDLW::Video::Renderer& renderer, Rect rect) {
-    renderer.SetDrawColor(rideing ? Color::White : Color::Gray);
-    renderer.FillRect(rect);
+    if(rideing) {
+        renderer.SetDrawColor(Color::Black);
+    } else {
+        renderer.SetDrawColor(Color::Green);
+    }
+    renderer.DrawLine(Point(0, 0), Point(rect.size.x, rect.size.y));
+    renderer.DrawLine(Point(0, rect.size.y), Point(rect.size.x, 0));
+    // TODO
+
 };
 
 }}
